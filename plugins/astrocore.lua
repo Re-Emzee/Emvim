8d1bfad36f8737f652d5c5f6f7b6668a65e4a293
Re-Emzee <molave.liggayu@gmail.com>
2025-02-12 23:58:44 -0800

    dap and code running (w/cursor and scrolling)

--- plugins/astrocore.lua
+++ plugins/astrocore.lua
@@ -58,6 +58,9 @@ return {
           desc = "Close buffer from tabline",
         },
 
+        ["<C-p>"] = { "<Plug>(YankyPreviousEntry)", desc = "Previous yank entry" },
+        ["<C-n>"] = { "<Plug>(YankyNextEntry)", desc = "Next yank entry" },
+
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
